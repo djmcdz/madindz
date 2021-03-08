a054e99ba1e763c93b5d1c5349b12b9683cf2145
AMISOFT <koudrimustapha@gmail.com>
2021-03-08 15:30:07 +0100

    Create  context.js

---
+++ context.js
@@ -0,0 +1,18 @@
+<div class="rainbow">  
+<div id="tb-subscribe-css">  
+<div class="tb-subscribe-wrapper">  
+<div class="tb-subscribe-wrapper-head">  
+اشترك في صحيفتنا الإخبارية. 
+</div>  
+اشترك في قائمتنا البريدية واحصل على تحديثات جديدة عالية الجودة مدى الحياة.<br />  
+<div class="tb-subscribe-form">  
+<form action="https://feedburner.google.com/fb/a/mailverify?uri=XXXXX" class="tb-subscribe-form" method="post" onsubmit="window.open('http://feeds.feedburner.com/blogspot/ZuEEJ', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" target="popupwindow">  
+<input name="uri" type="hidden" value="XXXXX" />  
+<input name="loc" type="hidden" value="en_US" />  
+<input autocomplete="off" class="tb-subscribe-css-email-field" name="email" placeholder="أدخل عنوان بريدك الالكتروني" />  
+<input class="tb-subscribe-css-button" title="" type="submit" value="شاركنا هنا" />  
+</form>  
+</div>  
+</div>  
+</div>  
+</div>
